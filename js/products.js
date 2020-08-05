924627e8722fc7e13d31a2f91a7df22c499a5939
slacruz95 <68952370+slacruz95@users.noreply.github.com>
2020-08-05 18:08:49 -0300

    Entrega 1.2 -Products

--- js/products.js
+++ js/products.js
@@ -1,6 +1,56 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+var productsArray = [];
+
+function showProducts(array){
+
+    let contenido= "";
+
+    for(let i=0; i<array.length; i++){
+
+        let product = array[i];
+        
+
+        contenido += `
+        <a href="product-info.html" class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.name + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name +`</h4>
+                        <small class="text-muted">` + "$"+product.cost + `</small>
+                    </div>
+                    <p class="mb-1">` + product.description + `</p>
+                </div>
+            </div>
+        </a>
+        `
+        
+        
+
+        document.getElementById("listadoProduct").innerHTML = contenido;
+    }
+}
+
+
+
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function(resultObjeto){
+       if (resultObjeto.status === "ok"){
+        productsArray = resultObjeto.data;
+
+        showProducts(productsArray);
+       }
+
+    });
 
 });
