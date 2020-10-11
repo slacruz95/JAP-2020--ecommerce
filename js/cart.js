c1dba12027a0523630bd123f8f04a2abb904d310
slacruz95 <68952370+slacruz95@users.noreply.github.com>
2020-10-11 17:26:39 -0300

    v1.11

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,81 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
+var productsArray = [];
+
+
+function total (){
+    let total = 0;
+    let subs = document.getElementsByClassName("subtotal");
+    for(let i=0; i < subs.length; i++){
+        total += parseInt(subs[i].innerHTML);
+    }
+    document.getElementById("sumaTotal").innerHTML = total;
+    
+} 
+
+function subtotal (unitCost, i){
+    let count = parseInt(document.getElementById(`cant${i}`).value);
+    subTotal = unitCost * count;
+    document.getElementById(`articleSubtotal${i}`).innerHTML = subTotal;
+    
+    total();
+}
+
+function currencyConvert(currency, cost){
+
+    if(currency === "UYU"){
+        return cost / 40;
+    }
+    else{
+      return cost
+    }
+
+    
+
+}
+
+
+function articlesShow(array){
+    let cont = "";
+    
+    for(let i=0; i < array.length; i++){
+
+        let articles = array[i];
+        let costD = currencyConvert(articles.currency, articles.unitCost)
+        let sub = costD *  articles.count;
+
+        cont += `
+        <tr>
+        <td><img src='${articles.src}' width="100px"></td>
+
+        <td>${articles.name}</td>
+        <td>U$S ${costD}</td>
+        <td><input style="width:60px;" onchange="subtotal(${costD}, ${i})" 
+        type="number" id="cant${i}" value="${articles.count}" min="1"></td>
+
+        <td><span class="subtotal" id="articleSubtotal${i}" style="font=weight:bold;">${sub}</span></td>
+
+        </tr>
+        <br>
+
+
+        `
+     document.getElementById("list").innerHTML = cont;
+
+    }
+    total();
+}
+
+
+
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData("https://japdevdep.github.io/ecommerce-api/cart/654.json").then(function(resultObject){
+        if(resultObject.status === "ok"){
+            
+            productsArray= resultObject.data.articles;
+
+            articlesShow(productsArray);
+           
+        }
+
+    });
 
 });
