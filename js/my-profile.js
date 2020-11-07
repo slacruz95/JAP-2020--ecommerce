5966cfb4bb6ac49b11c0ea688af53d1fd03c909d
slacruz95 <68952370+slacruz95@users.noreply.github.com>
2020-11-07 20:16:51 -0300

    v1.13

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,86 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+let btnProfile = document.getElementById('btn-profile');
+
+/*
+let validatedImg = true;
+function imgSave() {
+  if (imgInput.value.trim() === '') {
+    validatedImg = false;
+  }
+  if (validatedImg) {
+    localStorage.setItem('img-profile', JSON.stringify({ img: imgInput.value }));
+  }
+}
+*/
+
+btnProfile.addEventListener('click', function (e) {
+  var name = document.getElementById('name-profile');
+  var apellido = document.getElementById('ap-profile');
+  var age = document.getElementById('age-profile');
+  var tel = document.getElementById('tel-profile');
+  var mail = document.getElementById('mail-profile');
+  var imgInput = document.getElementById('url-profile');
+  let validated = true;
+
+  if (name.value.trim() === '') {
+    name.classList.add('is-invalid');
+    validated = false;
+  } else {
+    name.classList.remove('is-invalid');
+    name.classList.add('is-valid');
+  }
+  if (apellido.value.trim() === '') {
+    apellido.classList.add('is-invalid');
+    validated = false;
+  } else {
+    apellido.classList.remove('is-invalid');
+    apellido.classList.add('is-valid');
+  }
+
+  if (mail.value.trim() === '') {
+    mail.classList.add('is-invalid');
+    validated = false;
+  } else {
+    mail.classList.remove('is-invalid');
+    mail.classList.add('is-valid');
+  }
+
+  if (validated) {
+    localStorage.setItem(
+      'profile-user',
+      JSON.stringify({
+        name: name.value,
+        apellido: apellido.value,
+        age: age.value,
+        tel: tel.value,
+        mail: mail.value,
+        img: imgInput.value,
+      })
+    );
+  }
+  setTimeout(function () {
+    window.location = 'my-profile.html';
+  }, 500);
 });
+
+let userLog = localStorage.getItem('user-logged');
+if (userLog) {
+  let profile = localStorage.getItem('profile-user');
+
+  if (profile) {
+    profile = JSON.parse(profile);
+
+    if (profile.img != '') {
+      document.getElementById('img-profile').src = profile.img;
+    }
+    document.getElementById('url-profile').value = profile.img;
+    document.getElementById('name-profile').value = profile.name;
+    document.getElementById('ap-profile').value = profile.apellido;
+    document.getElementById('age-profile').value = profile.age;
+    document.getElementById('tel-profile').value = profile.tel;
+    document.getElementById('mail-profile').value = profile.mail;
+  }
+}
--- js/products.js
+++ js/products.js
@@ -1,9 +1,9 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const ORDER_ASC_BY_COST = "pag->PAG";
-const ORDER_DESC_BY_COST = "PAG->pag";
-const ORDER_DESC_BY_REL = "REL->rel";
+const ORDER_ASC_BY_COST = 'pag->PAG';
+const ORDER_DESC_BY_COST = 'PAG->pag';
+const ORDER_DESC_BY_REL = 'REL->rel';
 
 var productsArray = [];
 var minCost = undefined;
@@ -49,23 +49,19 @@ function sortProducts(criterio, array) {
 }
 
 function showProducts(array) {
-  let contenido = "";
+  let contenido = '';
 
   for (let i = 0; i < array.length; i++) {
     let product = array[i];
 
     if (
-      (minCost == undefined ||
-        (minCost != undefined && parseInt(product.cost) >= minCost)) &&
-      (maxCost == undefined ||
-        (maxCost != undefined && parseInt(product.cost) <= maxCost))
+      (minCost == undefined || (minCost != undefined && parseInt(product.cost) >= minCost)) &&
+      (maxCost == undefined || (maxCost != undefined && parseInt(product.cost) <= maxCost))
     ) {
-      if (
-        buscar == undefined ||
-        product.name.toLowerCase().indexOf(buscar) != -1
-      ) {
+      if (buscar == undefined || product.name.toLowerCase().indexOf(buscar) != -1) {
         contenido +=
           `
+          <div class="col-md-6 col-lg-4 p-2">
         <a href="product-info.html" class="list-group-item list-group-item-action">
             <div class="row">
                 <div class="col-3">
@@ -79,31 +75,33 @@ function showProducts(array) {
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">` +
           product.name +
-          " U$S " +
+          ' U$S ' +
           product.cost +
           `</h4>
-                        <small class="text-muted">` +
-          "" +
-          product.soldCount +
-          `</small>
+                        
                     </div>
                     <p class="mb-1">` +
           product.description +
           `</p>
+          <small class="text-muted">` +
+          'Cant. Vendidos ' +
+          product.soldCount +
+          `</small>
                 </div>
             </div>
         </a>
+        </div>
         `;
       }
     }
 
-    document.getElementById("listadoProduct").innerHTML = contenido;
+    document.getElementById('listadoProduct').innerHTML = contenido;
   }
 }
 
-document.addEventListener("DOMContentLoaded", function (e) {
+document.addEventListener('DOMContentLoaded', function (e) {
   getJSONData(PRODUCTS_URL).then(function (resultObjeto) {
-    if (resultObjeto.status === "ok") {
+    if (resultObjeto.status === 'ok') {
       productsArray = resultObjeto.data;
 
       showProducts(productsArray);
@@ -111,39 +109,39 @@ document.addEventListener("DOMContentLoaded", function (e) {
   });
 });
 
-document.getElementById("desc").addEventListener("click", function (e) {
+document.getElementById('desc').addEventListener('click', function (e) {
   productsArray = sortProducts(ORDER_DESC_BY_COST, productsArray);
-  document.getElementById("asc").classList.remove("active");
-  document.getElementById("rel").classList.remove("active");
+  document.getElementById('asc').classList.remove('active');
+  document.getElementById('rel').classList.remove('active');
 
   showProducts(productsArray);
 });
-document.getElementById("asc").addEventListener("click", function (e) {
+document.getElementById('asc').addEventListener('click', function (e) {
   productsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
-  document.getElementById("desc").classList.remove("active");
-  document.getElementById("rel").classList.remove("active");
+  document.getElementById('desc').classList.remove('active');
+  document.getElementById('rel').classList.remove('active');
 
   showProducts(productsArray);
 });
-document.getElementById("rel").addEventListener("click", function (e) {
+document.getElementById('rel').addEventListener('click', function (e) {
   productsArray = sortProducts(ORDER_DESC_BY_REL, productsArray);
-  document.getElementById("asc").classList.remove("active");
-  document.getElementById("desc").classList.remove("active");
+  document.getElementById('asc').classList.remove('active');
+  document.getElementById('desc').classList.remove('active');
 
   showProducts(productsArray);
 });
 
-document.getElementById("filter").addEventListener("click", function (e) {
-  minCost = document.getElementById("minRange").value;
-  maxCost = document.getElementById("maxRange").value;
+document.getElementById('filter').addEventListener('click', function (e) {
+  minCost = document.getElementById('minRange').value;
+  maxCost = document.getElementById('maxRange').value;
 
-  if (minCost != undefined && minCost != "" && parseInt(minCost) >= 0) {
+  if (minCost != undefined && minCost != '' && parseInt(minCost) >= 0) {
     minCost = parseInt(minCost);
   } else {
     minCost = undefined;
   }
 
-  if (maxCost != undefined && maxCost != "" && parseInt(maxCost) >= 0) {
+  if (maxCost != undefined && maxCost != '' && parseInt(maxCost) >= 0) {
     maxCost = parseInt(maxCost);
   } else {
     maxCost = undefined;
@@ -152,16 +150,16 @@ document.getElementById("filter").addEventListener("click", function (e) {
   showProducts(productsArray);
 });
 
-document.getElementById("clean").addEventListener("click", function () {
-  document.getElementById("minRange").value = "";
-  document.getElementById("maxRange").value = "";
+document.getElementById('clean').addEventListener('click', function () {
+  document.getElementById('minRange').value = '';
+  document.getElementById('maxRange').value = '';
 
   minCost = undefined;
   maxCost = undefined;
   showProducts(productsArray);
 });
 
-document.getElementById("buscador").addEventListener("input", function () {
-  buscar = document.getElementById("buscador").value.toLowerCase();
+document.getElementById('buscador').addEventListener('input', function () {
+  buscar = document.getElementById('buscador').value.toLowerCase();
   showProducts(productsArray);
 });
